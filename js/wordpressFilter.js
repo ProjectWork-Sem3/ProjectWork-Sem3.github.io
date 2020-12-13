0fd14029f56e4ab427cd99585b55ea8e1c5e7d7f
unknown <adityakumar.cs19@bmsce.ac.in>
2020-12-13 22:10:56 +0300

    tenth draft

--- js/wordpressFilter.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("body > div.footer-nav-widgets-wrapper.header-footer-group > div > aside > div > div.footer-widgets.column-one.grid-item")
-
-body > div.footer-nav-widgets-wrapper.header-footer-group > div > aside > div > div.footer-widgets.column-one.grid-item
